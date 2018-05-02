54e8e56d61ca0733cbe0e3c66ea4edc4d57f4086
André <andrevieiraexbyte@gmail.com>
2018-05-01 23:08:23 -0300

    Estilizando as views, configurando consulta e recuperação dados no BD

--- controle_alunos_nodejs/app/routes/alunos.js
+++ controle_alunos_nodejs/app/routes/alunos.js
@@ -3,10 +3,16 @@ var router = express.Router();
 
 var db = require('../db');
 
-router.get('/', (req, res, next) =>{
+router.get('/', (req, res, next) => {
+    db("alunos").then((alunos) => {
+        res.render('index', {
+            alunos: alunos
+        });
+    }, next);
 });
 
 router.get('/add', (req, res, next) =>{
+    res.render('add');
 });
 
 router.post('/', (req, res, next) =>{
